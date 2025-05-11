92ee7308a1990ac1a423ca3ba7315a7040fa8bea
Shah Sultan <sultan91234567@gmail.com>
2025-05-11 18:13:14 +0600

    Runs in O(n log(n)) time complexity     -previous was O(n^2)

--- Top 150 Interview/300_LIS.py
+++ Top 150 Interview/300_LIS.py
@@ -1,13 +1,26 @@
 
+import bisect
+
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        occurance=[]
-        occurance.append(1)
+        #But this method or sub[] doesn't contain correct order of LIS. Only give length. Time Complexity O(n*logn)
+        sub=[]
+        for num in nums:
+            index=bisect.bisect_left(sub,num)
+            if index==len(sub):
+                sub.append(num)
+            else:
+                sub[index]=num
         
-        for i in range(1,len(nums),1):
-            occurance.append(1)
-            for j in range(i):
-                if nums[i]>nums[j] and occurance[j]+1>occurance[i]:
-                    occurance[i]=occurance[j]+1
+        return len(sub)
+        # occurance=[]
+        # occurance.append(1)
+        
+        # for i in range(1,len(nums),1):
+        #     occurance.append(1)
+        #     for j in range(i):
+        #         if nums[i]>nums[j] and occurance[j]+1>occurance[i]:
+        #             occurance[i]=occurance[j]+1
                     
-        return max(occurance)
+        # return max(occurance)
